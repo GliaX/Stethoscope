6f57cc728a1b916828d03dffcc9bbfb9c1e1acac
Joachim Glauche <jglauche@tardis.(none)>
2014-12-05 21:57:47 +0100

    finished outside dimensons without tube exit

--- stethoscope_head/lib/printed/stethoscope_head.rb
+++ stethoscope_head/lib/printed/stethoscope_head.rb
@@ -4,10 +4,9 @@ class StethoscopeHead < CrystalScad::Printed
 
 	def show
 		# Dimensions of the Littmann Head 
-	
-
-
+		#
 		# 1. Outside dimensions, not including inner dimensions nor tubing exit
+	  #	
 
 		@z = 0
 	
@@ -20,13 +19,41 @@ class StethoscopeHead < CrystalScad::Printed
 		
 		@z += stage2				
 		# The third stage above the silicone ring of the diaphragm. This measures 1mm thick, but goes taper at some point. I cannot measure this precisely, so this might be actually thinner on the real model
-		res += cylinder(d: 44.3, h: stage3 = 0.8).translate(z:@z) 
+		res += cylinder(d: current_diameter = 44.3 , h: stage3 = 0.8).translate(z:@z) 
 						
 		@z += stage3				
-		# At stage 4, the head gets much thinner. I estimated the thinnest point at about 7.5mm from bottom 
-		res += cylinder(d1: 44.3, d2: 16.95, h: stage4 = 7.5 - @z).translate(z:@z) 
+
+		# I missed this angle earlier 
+		res += cylinder(d1: current_diameter, d2: current_diameter=21, h: stage4 = 6.2 - @z).translate(z:@z) 
+		@z += stage4
+
+		# At stage 5, the head gets much thinner. I estimated the thinnest point at about 7.5mm from bottom 
+		res += cylinder(d1: current_diameter, d2: current_diameter = 16.95, h: stage5 = 7.5 - @z).translate(z:@z) 
+		@z += stage5
 		
-		# FIXME: this actually has two angles.
+
+		# Going up to the smaller funnel 
+		res += cylinder(d1: current_diameter, d2: current_diameter = 27, h: stage6 = 21 - @z).translate(z:@z) 
+		@z += stage6
+	
+		# More upwards
+		res += cylinder(d1: current_diameter, d2: current_diameter = 32.75, h: stage7 = 25 - @z).translate(z:@z) 
+		@z += stage7
+		
+		# The upper silicone ring lip (note: diameter is estimated as I'm unable to measure it exactly)
+		res += cylinder(d: 31, h: stage8 = 25.8 - @z).translate(z:@z) 
+		@z += stage8
+		
+		# top
+		res += cylinder(d: 34.43, h: stage9 = 27.22 - @z).translate(z:@z) 
+		@z += stage9
+		
+
+		#
+		# 2. Inside dimensions, minus the tube exit
+	  #	
+
+
 
 	
 		res.color("lightgrey")
--- stethoscope_head/stethoscope_head.scad
+++ stethoscope_head/stethoscope_head.scad
@@ -1,5 +1,5 @@
 $fn=64;
-color("lightgrey"){union(){union(){union(){cylinder(h = 1.080, r = 21.715);
+color("lightgrey"){union(){union(){union(){union(){union(){union(){union(){union(){cylinder(h = 1.080, r = 21.715);
 translate(v = [0, 0, 1.080])
 cylinder(h = 1.060, r = 21.000);
 }
@@ -7,6 +7,21 @@ translate(v = [0, 0, 2.140])
 cylinder(h = 0.800, r = 22.150);
 }
 translate(v = [0, 0, 2.940])
-cylinder(h = 4.560, r1 = 22.150, r2 = 8.475);
+cylinder(h = 3.260, r1 = 22.150, r2 = 10.500);
+}
+translate(v = [0, 0, 6.200])
+cylinder(h = 1.300, r1 = 10.500, r2 = 8.475);
+}
+translate(v = [0, 0, 7.500])
+cylinder(h = 13.500, r1 = 8.475, r2 = 13.500);
+}
+translate(v = [0, 0, 21.000])
+cylinder(h = 4.000, r1 = 13.500, r2 = 16.375);
+}
+translate(v = [0, 0, 25.000])
+cylinder(h = 0.800, r = 15.500);
+}
+translate(v = [0, 0, 25.800])
+cylinder(h = 1.420, r = 17.215);
 }
 }
