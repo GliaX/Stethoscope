0849718978bb24d4afc1f41c20c1352f29691420
Jennifer Glauche <webmaster@joaz.de>
2015-04-02 06:56:49 +0200

    made a better shaped eartube

--- stethoscope_head/lib/printed/eartube.rb
+++ stethoscope_head/lib/printed/eartube.rb
@@ -1,62 +1,63 @@
-class Eartube < CrystalScad::Printed
-
-	# Eartube is the part that goes from a Y to split up the channels from the tubing towards an earpiece 
-	# that will be shaped with PCL for now
-	def initialize
-		# The tube length of the Littman version is about approximately 130mm until it gets joined with the
-		# splitting part
-		@tube_length = 155
-		
-		# Radius of the bent
-		@bent_radius = 25
-		
-		# Angle of the bent, must be between or equal 90 and 180°
-		@bent_angle = 125
+class EartubePipe < CrystalScad::Pipe
+	# inheriting Pipe here to create the design. We want a hexagonal outer diameter for easy printing
+	# and a circular inner diameter	
+	def initialize(args={})
+		super(args) # call initialize from CrystalScad::	
+	
 
 		# Inner diameter of the Littmann version is about 3mm, I'm going for a slightly bigger inner diameter
 		# here because of possible shrinkage and print imperfections
 		@inner_diameter = 3.7
 
 		# Outer diameter of the hexagonal outer diameter
-		@outer_diameter = @inner_diameter + 3 
+		@diameter = @inner_diameter + 3 
+	end
+
+	# This defines the outside shape of the pipe
+	def shape
+		@line_rotation = 30 # This is necessary to have hexagonal lines align with the angled ones
+		return circle(d:@diameter,fn:6) # hexagonal shape
+	end	
+
+	#	This defines the inside shape of the pipe
+	def inner_shape
+		circle(d:@inner_diameter)
+	end
+
+end
+
+class Eartube < CrystalScad::Printed
+
+	# Eartube is the part that goes from a Y to split up the channels from the tubing towards an earpiece 
+	# that will be shaped with PCL for now
+	def initialize		
+		# I have removed the previous parametric parameters in order to make it easier to model	
+		# in the show method. Values and details can be changed there
 	end
 
 
 	def part(show)
-		res = outside_shape(@tube_length)
-		res += bent(@bent_radius,@bent_angle)
-	end
+		# Initialize the pipe, we've defined the dimensions above in that class already		
+		pipe = EartubePipe.new
 
-	def outside_shape(length)
-		# hexagonal outside shape
-		res = cylinder(d:@outer_diameter,h:length,	fn:6)
-		# circular inner tube
-		res -= cylinder(d:@inner_diameter,h:length+0.2).translate(z:-0.1)	
-		# positioning it flat on surface		
-		res.rotate(x:90)	
-	end
+		# We start with the piece that goes into the ear
+		pipe.line(3) # 3mm line
+		# Create a Clockwise angle, which goes into the ear
+		pipe.cw(10,20) # radius, angle
 
-	def bent(radius,angle)	
-		unless angle >= 90 and angle <= 180		
-			raise "the bent method only supports angles between 90 and 180 degrees"
-		end
 
-		# so in order to make the angle in a tube, I'm going to rotary extrude the outside shape...
-		res = circle(d:@outer_diameter,fn:6).translate(x:radius).rotate_extrude(convexity:10)
-		# and then rotary extrude the inner shape 
-		res -= circle(d:@inner_diameter).translate(x:radius).rotate_extrude(convexity:10)
-		# As a result, we have a hollow ring. I'm using a cube that I'll intersect with the part
-		cut = cube([@outer_diameter*radius,@outer_diameter*radius,@outer_diameter*radius]).center_z
-		# the previous cube would create a 90° angle. If it's bigger than 90°, I'll add another cube to it like the one
-		# we used and rotate it along the Z axis by the angle, minus 90°
-		if angle > 90
-			cut += cube([@outer_diameter*radius,@outer_diameter*radius,@outer_diameter*radius]).center_z.rotate(z:angle-90)
-		end
-		# Finally we intersect the cut with the rotary extruded parts
-		res *= cut
+		pipe.cw(28,80) # radius, angle
 		
-		# Positioning it on 0 
-		res.translate(x:-radius)
+		pipe.cw(33,28) 
+
+		pipe.line(40)
+		pipe.cw(250,8)
+		pipe.line(40)
+		pipe.cw(85,10)
+		pipe.line(20)
+		
+
+		return pipe.pipe
 	end
 
 end
